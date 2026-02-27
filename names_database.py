fe28a30fcbc48166f206339d560812a3608d77b0
l9mm <ludvicksem@gmail.com>
2026-02-27 13:52:17 +0100

    Add files via upload

---
+++ names_database.py
@@ -0,0 +1,96 @@
+# Customer names database for review generation
+# Mix of realistic names from different backgrounds
+
+CUSTOMER_NAMES = [
+    # Common English names
+    "Sarah Johnson", "Michael Chen", "Emily Rodriguez", "James Anderson",
+    "Jessica Williams", "David Martinez", "Ashley Brown", "Christopher Davis",
+    "Amanda Wilson", "Matthew Taylor", "Jennifer Garcia", "Daniel Thomas",
+    
+    # International names
+    "Alex Kim", "Sophie Laurent", "Lucas Silva", "Emma Mueller",
+    "Ryan O'Connor", "Isabella Rossi", "Noah Patel", "Olivia Kowalski",
+    "Ethan Nguyen", "Ava Petrov", "Liam Zhang", "Mia Andersson",
+    
+    # Modern names
+    "Jordan Lee", "Taylor Smith", "Casey Morgan", "Riley Mitchell",
+    "Cameron White", "Avery Thompson", "Parker Jackson", "Quinn Harris",
+    "Hayden Martin", "Dakota Moore", "Peyton Clark", "Skylar Lewis",
+    
+    # Tech-savvy names
+    "Alex Turner", "Sam Rivera", "Morgan Foster", "Jamie Cooper",
+    "Tyler Brooks", "Casey Reynolds", "Drew Sullivan", "Blake Murphy",
+    "Spencer Bell", "Reese Bailey", "Kendall Price", "Sage Bennett",
+    
+    # Professional names
+    "Robert Evans", "Lisa Martinez", "Kevin Torres", "Rachel Hughes",
+    "Brandon Powell", "Michelle Gray", "Justin Russell", "Nicole Peterson",
+    "Steven Hayes", "Laura Jenkins", "Eric Foster", "Angela Coleman",
+    
+    # Gaming/Tech community names
+    "TechNinja47", "GameMaster_X", "ProBuilder99", "PCEnthusiast",
+    "RGBLover420", "FrameChaser", "OverclockKing", "SilentRunner",
+    "CoolBreezePC", "PixelPusher", "CodeWarrior", "ByteBeast" 
+
+    # Common English-style names
+    "Nathan Collins", "Chloe Bennett", "Andrew Parker", "Victoria Reed",
+    "Benjamin Carter", "Hannah Simmons", "Joshua Reed", "Madison Brooks",
+    "Logan Stewart", "Grace Coleman", "Aaron Bryant", "Zoe Richardson",
+
+    # International names
+    "Mateo Alvarez", "Elena Novak", "Tobias Schmidt", "Anya Ivanova",
+    "Marco Bianchi", "Leila Haddad", "Rafael Costa", "Ingrid Larsen",
+    "Dmitry Volkov", "Yuki Tanaka", "Arjun Mehta", "Fatima Zahra",
+
+    # Modern names
+    "Rowan Blake", "Emerson Hayes", "Finley Jordan", "Sawyer Ellis",
+    "Harper Quinn", "Phoenix Carter", "Lennon Gray", "River Bennett",
+    "Dakari Miles", "Sloane Harper", "Kai Morgan", "Ari Lennox",
+
+    # Tech-oriented names
+    "Elliot Grant", "Tristan Cole", "Devon Archer", "Micah Turner",
+    "Cody Barrett", "Jesse Vaughn", "Owen Fletcher", "Caleb Norton",
+    "Grant Holloway", "Nolan Pierce", "Victor Lang", "Simon Clarke",
+
+    # Professional style names
+    "Patrick Donovan", "Monica Ramirez", "Trevor Wallace", "Clara Benson",
+    "Marcus Delgado", "Vanessa Ortiz", "Derek Hoffman", "Natalie Briggs",
+    "Leonard Shaw", "Irene Castillo", "Phillip Harmon", "Cynthia Walsh",
+
+    # Gaming / Tech usernames
+    "ShadowCrafter", "TurboByte77", "HexaSlayer", "NovaStrikeX",
+    "QuantumGamer", "PixelRaider", "CryoBlaze", "MegaFrame",
+    "ZeroLatency", "UltraVortex", "SilentVector", "NextGenHero",
+    "CodePhantom", "RasterRider", "LogicHunter", "CircuitBreakerX"
+
+    # Common English-style names
+    "Connor McCarthy", "Ella Fitzgerald", "Jason Bradley", "Paige Montgomery",
+    "Calvin Armstrong", "Brooke Henderson", "Dylan Harper", "Sydney Lawson",
+    "Travis Bennett", "Faith Palmer", "Cole Davidson", "Jade Whitaker",
+
+    # International names
+    "Hugo Fernandes", "Katarina Pavlov", "Milan Jovanovic", "Lucia Romero",
+    "Omar Farouk", "Petra Horvat", "Sven Karlsson", "Alessia Romano",
+    "Viktor Orlov", "Mei Lin Zhao", "Rohan Desai", "Nadia El-Sayed",
+
+    # Modern names
+    "Zion Carter", "Amaya Brooks", "Jaxon Reed", "Lyric Monroe",
+    "Kendrix Lane", "Brielle Dawson", "Tatum Cross", "Zara Ellis",
+    "Kairo Hayes", "Alina Frost", "Maddox Rivers", "Nyla Stanton",
+
+    # Tech-oriented names
+    "Julian Mercer", "Colton Hayes", "Evan Sinclair", "Preston Wolfe",
+    "Gavin Rhodes", "Miles Donovan", "Spencer Knox", "Brent Callahan",
+    "Ian Prescott", "Wesley Boone", "Adrian Keller", "Dominic Sayers",
+
+    # Professional style names
+    "Harold Whitman", "Denise Crawford", "Glenn Patterson", "Marissa Vaughn",
+    "Curtis Manning", "Bianca Salazar", "Trevor Jennings", "Lydia Holmes",
+    "Stanley Porter", "Melanie Cross", "Darren Matthews", "Olga Kuznetsova",
+
+    # Gaming / Tech usernames
+    "HyperNovaX", "DarkPixelHero", "NanoStrike", "VoltageRush",
+    "CryoKnight", "OmegaCircuit", "PhantomFPS", "TurboQuantum",
+    "GlitchHunter", "BinarySamurai", "VortexSniper", "RapidShader",
+    "SteelFrameX", "NextWaveByte", "GhostLatency", "UltraCoreZ"
+]
